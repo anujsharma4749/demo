737b5b974727948df2f20252cf2295545748aeb9
Anuj <130233116+anujsharma4749@users.noreply.github.com>
2025-06-26 17:28:47 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from './assets/components/Card'
+ import ContactPage  from './assets/components/Contact'
+ import PrivacyPolicy from './assets/components/Privacy'
+export default function App() {
+  return (
+    <div>
+      <PrivacyPolicy/>
+    </div>
+  )
+}
+
